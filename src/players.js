bb6fc416984bb2e0ad8072259663eb746bf5b9f0
sana <benissia.fs@gmail.com>
2025-02-06 18:52:53 +0100

    v0

---
+++ src/players.js
@@ -0,0 +1,37 @@
+// src/players.js
+const players = [
+    {
+        name: "Ons Jabeur",
+        team: "Tunisia",
+        nationality: "Tunisian",
+        jerseyNumber: 1,
+        age: 29,
+        imageUrl: "https://th.bing.com/th/id/OIP.SeNFoWdurUcQy8gpJADsvQHaE8?rs=1&pid=ImgDetMain"
+    },
+    {
+        name: "Malek Jaziri",
+        team: "Tunisia",
+        nationality: "Tunisian",
+        jerseyNumber: 2,
+        age: 39,
+        imageUrl: "https://th.bing.com/th/id/R.4b902fe00e735545fb33ded9967a638a?rik=C%2fWViGBfSnDe%2fw&riu=http%3a%2f%2fkapitalis.com%2ftunisie%2fwp-content%2fuploads%2f2016%2f04%2fMalek-Jaziri.jpg&ehk=z4ygRXybPnW6DIg1p00bsu%2bmtpsw9XJSEIAyoLUPVqY%3d&risl=&pid=ImgRaw&r=0"
+    },
+    {
+        name: "Selima Sfar",
+        team: "Tunisia",
+        nationality: "Tunisian",
+        jerseyNumber: 3,
+        age: 46,
+        imageUrl: "https://raport.ba/wp-content/uploads/2023/09/Selima-Sfar-1024x576.jpg"
+    },
+    {
+        name: "Kamel-Eddine Fayad",
+        team: "Tunisia",
+        nationality: "Tunisian",
+        jerseyNumber: 4,
+        age: 28,
+        imageUrl: "https://www.tennismagazin.de/content/uploads/2014/08/245737.jpg"
+    }
+];
+
+export default players;  
